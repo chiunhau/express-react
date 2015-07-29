d279ac1ddb9004114f7b35ccc47f7cde39f17796
Chiunhau <chiunhau9@gmail.com>
2015-07-29 10:53:37 +0800

    react router done!

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var Router = require('react-router');
+var React = require('react');
 var app = express();
 
 // view engine setup
@@ -21,8 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
+
+app.use( function (req, res) {  
+  res.render('index', {title: "index"});
+});
 
 
 // catch 404 and forward to error handler
--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,4 +1,41 @@
 var React = require('react');
-var Container = require('./Container.jsx')
+var Router = require('react-router');
+var Route = Router.Route;	
+var RouteHandler = Router.RouteHandler;
+// var Content = require('./Content.jsx');
 
-React.render(<Container />, document.getElementById("content"));
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        <h1>Appppp</h1>
+        <RouteHandler />
+      </div>
+    );
+  }
+});
+
+var About = React.createClass({
+  render: function () {
+    return <h2>About</h2>;
+  }
+});
+
+var Login = React.createClass({
+  render: function () {
+    return <h2>Login</h2>;
+  }
+});
+
+var routes = (
+	<Route path="/" handler={App}>
+		<Route path="login" handler={Login} />
+		<Route path="about" handler={About} />
+	</Route>
+);
+
+
+
+Router.run(routes, Router.HistoryLocation, function(Handler, state) {
+  React.render(<Handler/>, document.body);
+});
